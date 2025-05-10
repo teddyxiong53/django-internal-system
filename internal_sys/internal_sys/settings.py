95c250929ea0c9d1bb4109954edf7a3b47a90a94
teddyxiong53 <1073167306@qq.com>
2025-05-10 16:48:42 +0800

    fix bug

--- internal_sys/internal_sys/settings.py
+++ internal_sys/internal_sys/settings.py
@@ -126,3 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = 'dashboard'
--- internal_sys/internal_sys/urls.py
+++ internal_sys/internal_sys/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', portal_views.dashboard, name='dashboard'),
     path('register/', portal_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='portal/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
